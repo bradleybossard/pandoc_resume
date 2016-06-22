ffde3730231651cac99f45affac843ea95f37a0a
Bradley Bossard <bradleybossard@gmail.com>
2016-06-22 15:25:40 +0000

    Adding images for resume

---
+++ images/resize.sh
@@ -0,0 +1,4 @@
+convert gumby-2011-hp.jpg -resize 25% gumby.png
+convert lunareclipse11-hp.png -resize 25% lunar.png
+convert startrek2012-hp.jpg -resize 25% startrek.png
+convert halloween-2011-hp.jpg -resize 25% halloween.png
