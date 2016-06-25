d708b63ddb0e025899911e568d48eb6c65bfb6db
Bradley Bossard <bradleybossard@gmail.com>
2016-06-25 01:40:58 +0000

    Adding images for portfolio section

--- images/resize.sh
+++ images/resize.sh
@@ -2,3 +2,12 @@ convert gumby-2011-hp.jpg -resize 25% gumby.png
 convert lunareclipse11-hp.png -resize 25% lunar.png
 convert startrek2012-hp.jpg -resize 25% startrek.png
 convert halloween-2011-hp.jpg -resize 25% halloween.png
+
+convert github-orig.png -resize 30x30 github.png
+convert codepen-orig.png -resize 30x30 codepen.png 
+convert stackoverflow-orig.png -resize 30x30 stackoverflow.png 
+convert docker-orig.png -resize 30x30 docker.png 
+convert codewars-orig.png -resize 30x30 codewars.png 
+convert hackerrank-orig.png -resize 30x30 hackerrank.png
+
+
