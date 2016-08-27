2775d4d61216cf21d8380e366616f4215a9be6da
Bradley Bossard <bradleybossard@gmail.com>
2016-08-27 20:45:49 +0000

    Added some cropping to improve profile icons

--- images/resize.sh
+++ images/resize.sh
@@ -1,14 +1,16 @@
-convert gumby-2011-hp.jpg -resize 10% gumby.png
-convert lunareclipse11-hp.png -resize 10% lunar.png
-convert startrek2012-hp.jpg -resize 10% startrek.png
-convert halloween-2011-hp.jpg -resize 10% halloween.png
+doodle_scale='10'
+profile_scale='60'
+convert gumby-2011-hp.jpg -resize $doodle_scale% gumby.png
+convert lunareclipse11-hp.png -resize $doodle_scale% lunar.png
+convert startrek2012-hp.jpg -resize $doodle_scale% startrek.png
+convert halloween-2011-hp.jpg -resize $doodle_scale% halloween.png
 
-convert github-orig.png -resize 60x60 github.png
-convert codepen-orig.png -resize 60x60 codepen.png 
-convert stackoverflow-orig.png -resize 60x60 stackoverflow.png 
-convert docker-orig.png -resize 60x60 docker.png 
-convert codewars-orig.png -resize 60x60 codewars.png 
-convert hackerrank-orig.png -resize 60x60 hackerrank.png
-convert linkedin-orig.png -resize 60x60 linkedin.png
+convert github-orig.png -resize ${profile_scale}x${profile_scale} github.png
+convert codepen-orig.png -resize ${profile_scale}x${profile_scale} codepen.png 
+convert stackoverflow-orig.png -resize ${profile_scale}x${profile_scale} stackoverflow.png 
+convert docker-orig.png -crop 200x175+30-20 -resize ${profile_scale}x${profile_scale} docker.png 
+convert codewars-orig.png -crop 180x180-0-0 -resize ${profile_scale}x${profile_scale} codewars.png 
+convert hackerrank-orig.png -resize ${profile_scale}x${profile_scale} hackerrank.png
+convert linkedin-orig.png -resize ${profile_scale}x${profile_scale} linkedin.png
 
 
